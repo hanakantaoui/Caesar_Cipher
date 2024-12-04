dd8ce862369002146b14ec88dae64a0727251591
hana <122279613+hanakantaoui@users.noreply.github.com>
2024-12-04 12:19:54 +0100

    CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -4,7 +4,7 @@ def caesar_cipher(text,key,mode):
     result=""
     if mode=="encrypt":
         key=key
-    else:
+    elif mode=="decrypt":
         key=(-key)
 
     for char in text:
@@ -16,11 +16,7 @@ def caesar_cipher(text,key,mode):
             result+=chr((ord(char)-start+key)%26+start)
         else:
             result+=char
-    return result
-
-#print("Encryption of HELLO :  ", caesar_cipher("HELLO",3,"encrypt"))    
-#print("Decryption of khoor : ",caesar_cipher("khoor",3,"decrypt")) 
-#print("Encryption of hello : ",caesar_cipher("hello",-3,"encrypt"))             
+    return result           
 
 text=input("\033[35m"+"Enter you text to encrypt or decrypt : "+"\033[0m")
 key=int(input("\033[35m"+"Please, enter your key : "+"\033[0m"))
